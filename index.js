21a4a399d185217a8e9c175bdc5ced6663cc7fe6
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-12 06:37:25 +0100

    launch app on successul db connect

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var { getEnv } = require('./src/utils/getEnv');
+var { db } = require('./src/configs/db');
 var { app } = require('./src/app');
 
-var port = getEnv('PORT', 4000);
+db.connect((err) => {
+    if(err) {
+        console.log(`Error connecting to database: `, err);
+        process.exit(1);
+    } else {
+        console.log(`Connected to database!`);
+        
+        var port = getEnv('PORT', 4000);
+        app.listen(port, () => {
+            console.log(`App running on port ${port}!`);
+        });
+    }
+})
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}!`);
-});
+
 
