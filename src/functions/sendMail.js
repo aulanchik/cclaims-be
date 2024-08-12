df157f3c7cb6e35e331177aeeb729d01ba687d6d
Artyom Ulanchik <artyom.u@outlook.com>
2024-08-12 06:36:25 +0100

    added send mail

---
+++ src/functions/sendMail.js
@@ -0,0 +1,17 @@
+var { mailgun } = require('../configs/mailgun');
+
+const sendMail = async (name, email, subject, html, text) => {
+    try {
+        const response = await mailgun.messages().send({
+            from: name,
+            to: email,
+            subject: subject,
+            html: html,
+            text: text
+        });
+    } catch (error) {
+        throw new Error("Error sending email: " + error);
+    }
+}
+
+module.exports = { sendMail };
